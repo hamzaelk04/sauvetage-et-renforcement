8bfafcf86cfa4ec1d198295e663312bea1c7a822
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-03-17 21:10:30 +0000

    sauvetage 1 in day 1 done

---
+++ Week 1/Day 1/sauvetage/S-Challenge-1.js
@@ -0,0 +1,15 @@
+const animals = [
+    'cat',
+    'dog',
+    'fish',
+    'sheep',
+    'cow'
+]
+
+console.log(animals);
+
+console.log(animals[0]);
+
+console.log(animals[animals.length - 1]);
+
+console.log(animals.length);
